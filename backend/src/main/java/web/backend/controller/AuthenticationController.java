9ecedba2518d26b00791b1c801651497b61c522c
murphyqwek <qwekinaft@list.ru>
2025-11-27 23:21:00 +0300

    Add AuthenticationController

---
+++ backend/src/main/java/web/backend/controller/AuthenticationController.java
@@ -0,0 +1,63 @@
+package web.backend.controller;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import web.backend.dto.UserCredentialsDTO;
+import web.backend.service.UserService;
+import web.backend.util.TokenUtil;
+
+@Path("/auth")
+public class AuthenticationController {
+    @Inject
+    private UserService userService;
+
+    @Inject
+    private TokenUtil tokenUtil;
+
+    @POST
+    @Path("/register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(UserCredentialsDTO userCredentials) {
+        try {
+            var Result = userService.register(userCredentials);
+
+            if (Result.isSuccess()) {
+                String token = tokenUtil.generateToken(userCredentials.getUsername());
+                return Response
+                        .status(Response.Status.CREATED)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                        .build();
+            }
+
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @POST
+    @Path("/register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(UserCredentialsDTO userCredentials) {
+        try {
+            var Result = userService.login(userCredentials);
+
+            if (Result.isSuccess()) {
+                String token = tokenUtil.generateToken(userCredentials.getUsername());
+                return Response
+                        .ok()
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                        .build();
+            }
+
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}

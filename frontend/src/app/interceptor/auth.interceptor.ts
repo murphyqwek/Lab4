5da1729003a9ffd942788a80cc8a3f90a092b420
murphyqwek <qwekinaft@list.ru>
2025-11-28 12:33:27 +0300

    Add interceptor if api method returns unauthorized redirect to login page

---
+++ frontend/src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,23 @@
+// auth.interceptor.ts
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AuthService } from '../services/AuthService';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout(); 
+        
+        router.navigate(['/login']);
+      }    
+      return throwError(() => error);
+    })
+  );
+};
